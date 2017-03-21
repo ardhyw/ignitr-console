df67d8e50b04e360836efc4d2073989b54ce15d6
Ardhy W. Nugroho <ardhyw@MasBookPro.local>
2017-03-21 17:20:33 +0700

    implement editable label for timing table values

--- gui.py
+++ gui.py
@@ -2,17 +2,19 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from editablelabel import EditableLabel
 
 class TimingTable(GridLayout):
-    
+
     def __init__(self, **kw):
         maxx = 9
         maxy = 5
         kw['rows'] = maxy
         kw['cols'] = maxx
         super(TimingTable, self).__init__(**kw)
-        
-        self.add_widget(Label(text="Table"))
+
+        self.add_widget(Label(text='Table'))
         for x in xrange(maxx-1):
             self.add_widget(Label(text='RPM'))
 
@@ -21,7 +23,7 @@ class TimingTable(GridLayout):
 
             for x in xrange(maxx-1):
                 t = str(x), str(y)
-                self.add_widget(Label(text=','.join(t)))
+                self.add_widget(EditableLabel(text=''.join(t)))
 
 class RootWidget(BoxLayout):
     pass
@@ -30,5 +32,5 @@ class Gui(App):
     def build(self):
         return RootWidget()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     Gui().run()
