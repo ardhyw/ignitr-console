8ee0c5266f296f48e1329d49e9395e7dfacd424f
Ardhy W. Nugroho <ardhyw@MasBookPro.local>
2017-03-21 14:30:24 +0700

    Dash board view with dummy values

---
+++ gui.py
@@ -0,0 +1,34 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+
+class TimingTable(GridLayout):
+    
+    def __init__(self, **kw):
+        maxx = 9
+        maxy = 5
+        kw['rows'] = maxy
+        kw['cols'] = maxx
+        super(TimingTable, self).__init__(**kw)
+        
+        self.add_widget(Label(text="Table"))
+        for x in xrange(maxx-1):
+            self.add_widget(Label(text='RPM'))
+
+        for y in xrange(maxy-1):
+            self.add_widget(Label(text='Load'))
+
+            for x in xrange(maxx-1):
+                t = str(x), str(y)
+                self.add_widget(Label(text=','.join(t)))
+
+class RootWidget(BoxLayout):
+    pass
+
+class Gui(App):
+    def build(self):
+        return RootWidget()
+
+if __name__ == "__main__":
+    Gui().run()
